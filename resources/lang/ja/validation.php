fd1f75af9746801f72290e2d4fc9ee77e92964ce
yamazakiyoshiki <yoshiky1129@gmail.com>
2024-09-10 00:06:04 +0900

    Pagination created

---
+++ resources/lang/ja/validation.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'required' => ':attribute は必須です。',
+    'email' => ':attribute には有効なメールアドレスを指定してください。',
+    'max' => [
+        'string' => ':attribute は:max文字以内で指定してください。',
+    ],
+    'min' => [
+        'string' => ':attribute は:min文字以上で指定してください。',
+    ],
+    'attributes' => [
+        'name' => '名前',
+        'email' => 'メールアドレス',
+        'password' => 'パスワード',
+    ],
+];
